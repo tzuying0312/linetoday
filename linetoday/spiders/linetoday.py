503e942f47fc1826d6b9af2a4b7d517f4a30f843
tzuying <tzuying0312@gmail.com>
2021-01-21 16:44:07 +0800

    1/21

---
+++ linetoday/spiders/linetoday.py
@@ -0,0 +1,21 @@
+
+from linetoday.items import PostItem
+from bs4 import BeautifulSoup
+import scrapy
+import time
+
+
+class LinetodaySpider(scrapy.Spider):
+    name = 'linetoday' #scrapy crawl linetoday -o output.json
+    start_urls = ['https://today.line.me/tw/v2/tab/domestic']
+
+    def parse(self, response):
+        item = PostItem()
+        soup = BeautifulSoup(response.text,'lxml')
+        post_list = soup.find_all('a',class_='articleCard articleCard--horizontal')
+        for i in post_list:
+            temp = i.getText().split('\n')[1:]
+            item['title'] = temp[0].replace(' ','')
+            item['source'] = temp[1].replace(' ','')
+            item['url'] = i['href']
+            yield item
