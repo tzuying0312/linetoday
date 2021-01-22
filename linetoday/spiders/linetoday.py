e771a5745b8f7446a11e1eefc5bb06b8d3fe09be
tzuying <tzuying0312@gmail.com>
2021-01-22 09:54:07 +0800

    1/22

--- linetoday/spiders/linetoday.py
+++ linetoday/spiders/linetoday.py
@@ -10,6 +10,10 @@ class LinetodaySpider(scrapy.Spider):
     start_urls = ['https://today.line.me/tw/v2/tab/domestic']
 
     def parse(self, response):
+        # item['url'] = response.css("a.articleCard articleCard--horizontal::attr(href)")[0].extract()
+        # item['title'] = response.css("a::attr(href)")[0].extract()
+        # item['source'] = response.css("a::attr(href)")[0].extract()
+        
         item = PostItem()
         soup = BeautifulSoup(response.text,'lxml')
         post_list = soup.find_all('a',class_='articleCard articleCard--horizontal')
