c8e2b7d0fab7af14b36ffe6032b81e6e1d9a21c1
tzuying <tzuying0312@gmail.com>
2021-01-24 15:56:53 +0800

    1/24

--- linetoday/spiders/linetoday.py
+++ linetoday/spiders/linetoday.py
@@ -9,13 +9,20 @@ class LinetodaySpider(scrapy.Spider):
     name = 'linetoday' #scrapy crawl linetoday -o output.json
     start_urls = ['https://today.line.me/tw/v2/tab/domestic']
 
-    def parse(self, response):     
+    def parse(self, response):  
         item = PostItem()
-        soup = BeautifulSoup(response.text,'lxml')
-        post_list = soup.find_all('a',class_='articleCard articleCard--horizontal')
-        for i in post_list:
-            temp = i.getText().split('\n')[1:]
-            item['title'] = temp[0].replace(' ','')
-            item['source'] = temp[1].replace(' ','')
-            item['url'] = i['href']
+        for post in response.css('section > div > a'):
+            item['title'] = (post.css('div.articleCard-content > span::text')[0].extract()).replace(' ','').replace('\n','')
+            item['source'] = post.css('div.articleCard-content > div > span::text')[0].extract()
+            item['url'] = post.css('a::attr(href)')[0].extract()
             yield item
+
+        # item = PostItem()
+        # soup = BeautifulSoup(response.text,'lxml')
+        # post_list = soup.find_all('a',class_='articleCard articleCard--horizontal')
+        # for i in post_list:
+        #     temp = i.getText().split('\n')[1:]
+        #     item['title'] = temp[0].replace(' ','')
+        #     item['source'] = temp[1].replace(' ','')
+        #     item['url'] = i['href']
+        #     yield item
